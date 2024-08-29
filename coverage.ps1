94923e36d5168358b2383464bef01790ad975db7
tokeiya3 <netseed@seedcreate.onmicrosoft.com>
2024-08-30 00:00:48 +0900

    Update coverage.ps1 to set RUSTFLAGS based on toolchain
    Added logic to conditionally set RUSTFLAGS based on the current Rust toolchain. This ensures that the appropriate flags are used for instrumentation and profiling, and throws an error for unexpected toolchains.

--- coverage.ps1
+++ coverage.ps1
@@ -9,10 +9,22 @@ cargo clean
 
 Get-ChildItem -Path ./coverage_prof -Recurse | Where-Object { $_.Name -ne ".gitkeep" } | Remove-Item -Recurse -Force
 Get-ChildItem -Path ./coverage -Recurse | Where-Object { $_.Name -ne ".gitkeep" } | Remove-Item -Recurse -Force
-
-
-
 Write-Host "Start coverage"
+
+$toolchain = rustup default
+
+if ($toolchain -eq "nightly-x86_64-pc-windows-msvc (default)")
+{
+    $env:RUSTFLAGS = "-Cinstrument-coverage"
+}
+elseif ($toolchain -eq "my-nightly (default)")
+{
+    $env:RUSTFLAGS = "-Zprofile -Ccodegen-units=1 -Copt-level=0 -Clink-dead-code -Coverflow-checks=off -Zpanic_abort_tests -Cpanic=abort"
+}
+else
+{
+    throw "Err:Unexpected toolchain."
+}
 
 if ($p)
 {
@@ -24,8 +36,7 @@ if ($m)
     Write-Host ("Module :{0}" -f $m)
 }
 
-$env:CARGO_INCREMENTAL=0
-$env:RUSTFLAGS="-Zprofile -Ccodegen-units=1 -Copt-level=0 -Clink-dead-code -Coverflow-checks=off -Zpanic_abort_tests -Cpanic=abort"
+$env:CARGO_INCREMENTAL = 0
 $env:LLVM_PROFILE_FILE = "../coverage_prof/traq-bot-http-rs-%p-%m.profraw"
 
 
@@ -49,6 +60,8 @@ else
 
 grcov . -s . --binary-path ./target/debug/ -t html --branch --ignore-not-existing -o ./coverage/
 
+
+Remove-Item ENV:CARGO_INCREMENTAL
 Remove-Item ENV:RUSTFLAGS
 Remove-Item ENV:LLVM_PROFILE_FILE
 
