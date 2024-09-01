43a3ce9c82a5145a9aafa6105ddbd8d8fc43ba24
tokeiya3 <netseed@seedcreate.onmicrosoft.com>
2024-09-01 20:25:45 +0900

    Add TypedValue and Value modules
    Introduce the TypedValue enum with various data type variants and conversion implementations. Added the Value struct to encapsulate TypedValue with raw string representation, along with placeholder methods.

--- ndjson/src/lib.rs
+++ ndjson/src/lib.rs
@@ -1,3 +1,5 @@
 pub mod number;
 pub mod parse_number_error;
 pub mod parser;
+mod typed_value;
+mod value;
---
+++ ndjson/src/value.rs
@@ -0,0 +1,16 @@
+use super::typed_value::TypedValue;
+
+pub struct Value {
+	typed_value: TypedValue,
+	raw: String,
+}
+
+impl Value {
+	pub fn from_str(value: Value, raw: &str) -> Self {
+		todo!()
+	}
+
+	pub fn from_string(value: Value, raw: String) -> Self {
+		todo!()
+	}
+}
