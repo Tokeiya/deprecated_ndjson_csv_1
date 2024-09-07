10fee1e512000833ae845dd9e0b6235271c54cf9
Tokeiya <netseed@seedcreate.onmicrosoft.com>
2024-09-07 09:49:43 +0900

    sync

---
+++ ndjson/src/number_value.rs
@@ -0,0 +1,29 @@
+use super::number::Number;
+use super::text_presentation::TextPresentation;
+
+pub struct NumberValue {
+	number: Number,
+	raw_string: String,
+}
+
+impl NumberValue {
+	pub fn new(number: Number, raw_string: String) -> Self {
+		todo!()
+	}
+
+	pub fn value(&self) -> &Number {
+		todo!()
+	}
+}
+
+impl TextPresentation for NumberValue{
+	fn build_raw(&self, buffer: &mut String) {
+		todo!()
+	}
+
+	fn build_trimmed(&self, buffer: &mut String) {
+		todo!()
+	}
+}
+
+#[cfg(test)]
