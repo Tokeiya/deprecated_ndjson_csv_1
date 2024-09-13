b2f104c2cc51a45a4496aa1e676d316a26504130
tokeiya3 <netseed@seedcreate.onmicrosoft.com>
2024-09-13 18:03:40 +0900

    Add hash test for StringValue
    Introduce a new test to verify the Hash trait implementation for StringValue. This ensures that differently formatted but equivalent strings produce the same hash value.

--- ndjson/src/elements/string_value.rs
+++ ndjson/src/elements/string_value.rs
@@ -1,6 +1,5 @@
 use super::with_raw_text::WithRawText;
 use std::hash::{Hash, Hasher};
-//pub struct StringValue(String);
 pub type StringValue = WithRawText<String>;
 
 impl Hash for StringValue {
@@ -21,6 +20,21 @@ impl Eq for StringValue {}
 mod test {
 	use super::*;
 
+	#[test]
+	fn hash() {
+		let a = StringValue::new("key".to_string(), r#"     "key"        "#.to_string());
+		let b = StringValue::new("key".to_string(), r#""key""#.to_string());
+
+		let mut hasher = std::collections::hash_map::DefaultHasher::new();
+		a.hash(&mut hasher);
+		let a_hash = hasher.finish();
+
+		let mut hasher = std::collections::hash_map::DefaultHasher::new();
+		b.hash(&mut hasher);
+		let b_hash = hasher.finish();
+
+		assert_eq!(a_hash, b_hash)
+	}
 	#[test]
 	fn eq() {
 		let a = StringValue::new("key".to_string(), r#"     "key"        "#.to_string());
