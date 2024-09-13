596778ac0438b5a2ed234cd9769ac73783b74ba3
tokeiya3 <netseed@seedcreate.onmicrosoft.com>
2024-09-13 17:55:46 +0900

    Remove commented-out trim functions and their tests
    Cleaned up the `white_space.rs` file by removing obsolete trim functions and their associated commented-out test cases. This helps streamline the code and reduce clutter, making maintenance easier.

--- ndjson/src/parser/white_space.rs
+++ ndjson/src/parser/white_space.rs
@@ -18,24 +18,6 @@ pub fn ws<I: Stream<Token = char>>() -> impl Parser<I, Output = String> {
 	cmb::many::<String, I, _>(tmp)
 }
 
-// pub fn trim_left<I: Stream<Token = char>>(
-// 	parser: impl Parser<I, Output = String>,
-// ) -> impl Parser<I, Output = TrimmedOutput> {
-// 	(ws(), parser).map(|(w, p)| TrimmedOutput::new(format!("{w}{p}")))
-// }
-//
-// pub fn trim_right<I: Stream<Token = char>>(
-// 	parser: impl Parser<I, Output = String>,
-// ) -> impl Parser<I, Output = TrimmedOutput> {
-// 	(parser, ws()).map(|(p, w)| TrimmedOutput::new(format!("{p}{w}")))
-// }
-//
-// pub fn trim<I: Stream<Token = char>>(
-// 	parser: impl Parser<I, Output = String>,
-// ) -> impl Parser<I, Output = TrimmedOutput> {
-// 	(ws(), parser, ws()).map(|(l, v, r)| TrimmedOutput::new(format!("{}{}{}", l, v, r)))
-// }
-
 #[cfg(test)]
 mod test {
 	use super::super::trimmed_output::test_helper::{assert, WS};
@@ -55,42 +37,4 @@ mod test {
 		assert_eq!(p, "");
 		assert_eq!(r, "aaa");
 	}
-
-	// #[test]
-	// fn trim_left() {
-	// 	let input = format!("{}value{}", WS.as_str(), WS.as_str());
-	// 	let p = chr::string::<&str>("value").map(|x| x.to_string());
-	// 	let mut p = super::trim_left(p);
-	// 	let fixture = p.parse(input.as_str()).unwrap();
-	//
-	// 	let (f, r) = p.parse(input.as_str()).unwrap();
-	//
-	// 	assert(&fixture, Some(&WS), &format!("{}value", WS.as_str()));
-	// }
-	//
-	// #[test]
-	// fn trim_right() {
-	// 	let input = format!("{}value{}", WS.as_str(), WS.as_str());
-	// 	let p = chr::string::<&str>("value").map(|x| x.to_string());
-	// 	let mut p = super::trim_right(p);
-	//
-	// 	assert!(p.parse(&input).is_err());
-	//
-	// 	let input = format!("value{}", WS.as_str());
-	//
-	// 	let p = chr::string::<&str>("value").map(|x| x.to_string());
-	// 	let mut p = super::trim_right(p);
-	//
-	// 	let fixture = p.parse(&input).unwrap();
-	// 	assert(&fixture, None, &input);
-	// }
-	//
-	// #[test]
-	// fn trim() {
-	// 	let input = format!("{}{}value{}", WS.as_str(), WS.as_str(), WS.as_str());
-	// 	let p = chr::string::<&str>("value").map(|x| x.to_string());
-	// 	let mut p = super::trim(p);
-	// 	let fixture = p.parse(input.as_str()).unwrap();
-	// 	assert(&fixture, None, &input);
-	// }
 }
