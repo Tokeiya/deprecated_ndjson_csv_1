76f8273aa6ee94b8fa0a2f60ec2ba311b2fa5a93
tokeiya3 <netseed@seedcreate.onmicrosoft.com>
2024-09-15 08:47:01 +0900

    Add file output to text_expression test helper
    Enhanced `dump_to_file` to write text to files with timestamped names in the `../artifact` directory. This improves debugging by allowing easy inspection of test outputs.

--- ndjson/src/elements/text_expression.rs
+++ ndjson/src/elements/text_expression.rs
@@ -6,5 +6,14 @@ pub trait TextExpression {
 #[cfg(test)]
 pub mod test_helper {
 	use chrono::prelude::*;
-	pub fn dump_to_file(text: &str) {}
+	use std::fs::File;
+	use std::io::prelude::*;
+
+	pub fn dump_to_file(text: &str) {
+		let path = format!("../artifact/{}.txt", Local::now().format("%b_%d_%H_%M"));
+		let mut file = File::create(&path).unwrap();
+
+		_ = file.write_all(text.as_bytes()).unwrap();
+	}
 }
+
