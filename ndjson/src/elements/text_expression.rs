df555f177b456568f2c93b501874a1e869ae7baf
tokeiya3 <netseed@seedcreate.onmicrosoft.com>
2024-09-15 13:35:44 +0900

    Add assert_text_expression test helper
    Introduces a new helper function `assert_text_expression` to the `test_helper` module. This function simplifies assertions by comparing the raw text output of `TextExpression` implementations with expected values. This enhances test readability and reduces boilerplate code in test cases.

--- ndjson/src/elements/text_expression.rs
+++ ndjson/src/elements/text_expression.rs
@@ -9,15 +9,24 @@ pub trait TextExpression {
 
 #[cfg(test)]
 pub mod test_helper {
+	use super::*;
 	use chrono::prelude::*;
 	use std::fs::File;
 	use std::io::prelude::*;
-
 	pub fn dump_to_file(text: &str) {
 		let path = format!("../artifact/{}.txt", Local::now().format("%b_%d_%H_%M"));
 		let mut file = File::create(&path).unwrap();
 
 		_ = file.write_all(text.as_bytes()).unwrap();
+	}
+
+	pub fn assert_text_expression<T: TextExpression>(actual: &dyn TextExpression, expected: &str) {
+		let mut buff = String::new();
+
+		actual.build_raw_text(&mut buff);
+		assert_eq!(buff, expected);
+
+		assert_eq!(actual.raw_text(), expected)
 	}
 }
 
